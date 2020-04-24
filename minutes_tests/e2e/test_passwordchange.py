4c943a24995e584ed8fbe5101e43b754da76fd43
Jonatan Zint <jonatan@ctrl.alt.coop>
2020-04-24 23:41:35 +0200

    add API and tests for retrieving token and changing passowrd

---
+++ minutes_tests/e2e/test_passwordchange.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.models import User
+from django.test import LiveServerTestCase
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+
+INITIAL_PASSWORD = 'initialpassword'
+CHANGED_PASSWORD = 'changedpassword'
+
+
+class PasswordChangeTest(LiveServerTestCase):
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = User.objects.create(username='testuser')
+        self.user.set_password(INITIAL_PASSWORD)
+        self.user.save()
+        self.user_token = Token.objects.create(user=self.user)
+
+    def test_401_if_not_authenticated(self):
+        response = self.client.post('/api/v1/changepassword/', {})
+        self.assertEqual(response.status_code, 401)
+
+    def test_400_if_not_providing_old_password(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_token.key)
+        response = self.client.post('/api/v1/changepassword/', {'new_password': CHANGED_PASSWORD})
+        self.assertEqual(response.status_code, 400)
+
+    def test_403_if_providing_wrong_old_password(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_token.key)
+        response = self.client.post(
+            '/api/v1/changepassword/',
+            {'old_password': 'notcorrect', 'new_password': CHANGED_PASSWORD}
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_200_for_correct_change_request_and_verify_new_password(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_token.key)
+        response = self.client.post(
+            '/api/v1/changepassword/',
+            {'old_password': INITIAL_PASSWORD, 'new_password': CHANGED_PASSWORD}
+        )
+        self.assertEqual(response.status_code, 204)
+        response = self.client.post(
+            '/api/v1/token/',
+            {'username': self.user.username, 'password': INITIAL_PASSWORD}
+        )
+        self.assertEqual(response.status_code, 400)
+        response = self.client.post(
+            '/api/v1/token/',
+            {'username': self.user.username, 'password': CHANGED_PASSWORD}
+        )
+        self.assertEqual(response.status_code, 200)
+
