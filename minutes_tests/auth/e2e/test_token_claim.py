d8b150d1192904c1a2b15fb53e6611a5cf4986fa
Jonatan Zint <jonatan@ctrl.alt.coop>
2020-04-26 20:54:02 +0200

    fix lint issue

--- minutes_tests/auth/e2e/test_token_claim.py
+++ minutes_tests/auth/e2e/test_token_claim.py
@@ -44,7 +44,6 @@ class TokenAuthenticationTest(LiveServerTestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-
     def test_400_for_expired_token(self):
         self.user_claim_token.expires = timezone.now() - timezone.timedelta(seconds=1)
         self.user_claim_token.save()
